2e85d6fccfee25dd17e63d6b9a07fa5978424d5b
lokomass <thelofoforadrummer@hotmail.com>
2024-07-25 15:54:31 +0200

    url.sh

--- url.sh
+++ url.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-declare -A NAS
-declare -A URL
-NAS[DNS]="nas.home"
-NAS[IP]="192.168.100.150"
-URL[IP]="http://${NAS[IP]}"
-URL[DNS]="http://${NAS[DNS]}"
-URL[PROXMOX]="${URL[IP]}/proxmox"
-URL[SCRIPTS]="${URL[DNS]}/scripts"
+NAS_DNS="nas.home"
+NAS_IP="192.168.100.150"
+URL_IP="http://${NAS_IP}"
+URL_DNS="http://${NAS_DNS}"
+URL_PROXMOX="${URL_IP}/proxmox"
+URL_SCRIPTS="${URL_DNS}/scripts"
 
