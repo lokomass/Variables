0d9c870cd4e36d900ca4cff3d085f2ef5a7a1bab
lokomass <thelofoforadrummer@hotmail.com>
2024-07-25 15:54:52 +0200

    url.sh

--- url.sh
+++ url.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-NAS_DNS="nas.home"
-NAS_IP="192.168.100.150"
-URL_IP="http://${NAS_IP}"
-URL_DNS="http://${NAS_DNS}"
-URL_PROXMOX="${URL_IP}/proxmox"
-URL_SCRIPTS="${URL_DNS}/scripts"
+export NAS_DNS="nas.home"
+export NAS_IP="192.168.100.150"
+export URL_IP="http://${NAS_IP}"
+export URL_DNS="http://${NAS_DNS}"
+export URL_PROXMOX="${URL_IP}/proxmox"
+export URL_SCRIPTS="${URL_DNS}/scripts"
 
