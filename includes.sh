1bacb25929fdb635b3318d74bfa0b80ec9b651f9
lokomass <thelofoforadrummer@hotmail.com>
2024-07-27 13:21:52 +0200

    scripts

--- includes.sh
+++ includes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILES=$(curl -s "https://api.github.com/repos/lokomass/variables/contents/files" | jq -r ".[] | .download_url")
-for FILE in "${FILES[@]}"
+for FILE in $FILES
 do
-	source <(curl -s "https://raw.githubusercontent.com/lokomass/variables/main/${FILE}.sh")
+	source <(curl -s "$FILE")
 done
 
