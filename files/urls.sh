989c03360aefa630f77d236ffb9cbf0001fd63c3
lokomass <thelofoforadrummer@hotmail.com>
2024-07-27 13:52:58 +0200

    scripts

--- files/urls.sh
+++ files/urls.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export LOCAL_NETWORK="192.168.100"
+export NAS_IP="$LOCAL_NETWORK.150"
 export NAS_DNS="nas.home"
-export NAS_IP="192.168.100.150"
-export URL_IP="http://${NAS_IP}"
-export URL_DNS="http://${NAS_DNS}"
-export URL_PROXMOX="${URL_IP}/proxmox"
-export URL_SCRIPTS="${URL_DNS}/scripts"
+export URL_IP="http://$NAS_IP"
+export URL_DNS="http://$NAS_DNS"
+export URL_PROXMOX="$URL_IP/proxmox"
+export URL_SCRIPTS="$URL_DNS/scripts"
 
--- includes.sh
+++ includes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-FILES=$(curl -s "https://api.github.com/repos/lokomass/variables/contents/files" | jq -r ".[] | .download_url")
+URL="https://api.github.com/repos/lokomass/variables/contents/files"
+FILES=$(curl -s "$URL" | jq -r ".[] | .download_url")
 for FILE in $FILES
 do
 	source <(curl -s "$FILE")
